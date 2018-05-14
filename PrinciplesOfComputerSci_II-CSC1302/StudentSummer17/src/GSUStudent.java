0af53ffb0a7d9e02e78f0176c145de148316b7b6
YangLiCODES <33105065+YangLiCODES@users.noreply.github.com>
2018-05-14 17:06:01 -0400

    Add files via upload

---
+++ PrinciplesOfComputerSci_II-CSC1302/StudentSummer17/src/GSUStudent.java
@@ -0,0 +1,17 @@
+
+public class GSUStudent {
+/*blueprint L06-CH08 in class practice
+ * 
+ */
+	public static void main(String[] args) {
+		Student Jone= new Student();
+		Jone.name="Jone A";
+		Jone.Gpa=3.9;
+		
+		Student Tom=new Student();
+		Tom.Gpa=3.6;
+		
+		System.out.println(Jone.name+" GPA is "+Jone.Gpa);
+	}
+
+}
---
+++ PrinciplesOfComputerSci_II-CSC1302/StudentSummer17/src/Student.java
@@ -0,0 +1,6 @@
+
+public class Student {
+	double Gpa;
+	String name;
+}
+
